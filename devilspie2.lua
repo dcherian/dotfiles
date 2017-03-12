39eacf220d309f32dff33fc7412b1c7abde8ecef
deepak <dpak.cherian@gmail.com>
2017-03-12 12:11:41 -0700

    Some fixes; add devilspie & reset-touchpad.

---
+++ devilspie2.lua
@@ -0,0 +1,12 @@
+-- the debug_print command does only print anything to stdout
+-- if devilspie2 is run using the --debug option
+debug_print("Window Name: " .. get_window_name());
+debug_print("Application name: " .. get_application_name())
+
+if (get_application_name() == "Rhythmbox") or (get_application_name() == "Signal Private Messenger") or (get_application_name() == "emacs-snapshot")then
+   pin_window();
+end
+
+if (get_application_name() == "Thunderbird") or (get_application_name() == "Shotwell") then
+   set_window_workspace(1);
+end
