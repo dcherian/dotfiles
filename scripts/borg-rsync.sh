fd51f3ea6e4c58f3747a465652b099cbabc8bbb7
deepak <dpak.cherian@gmail.com>
2017-08-23 09:23:46 -0700

    add notes folder to rsync backup

--- scripts/borg-rsync.sh
+++ scripts/borg-rsync.sh
@@ -9,7 +9,9 @@ export BORG_PASSPHRASE='gxz!0lopborgdarya'
 borg create --remote-path=borg1 -v --stats \
      $REPOSITORY::'{now:%Y-%m-%d}'  \
      /home/deepak/website \
-     /home/deepak/Documents
+     /home/deepak/Documents \
+     /home/deepak/org \
+     /home/deepak/notes
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. The '{hostname}-' prefix is very important to
