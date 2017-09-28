c518d43a14ece92165351532f1b0f67d5de7697b
deepak <dpak.cherian@gmail.com>
2017-09-28 10:12:14 -0700

    autoreload in ipython config.

--- ipython_config.py
+++ ipython_config.py
@@ -1,3 +1,6 @@
+c = get_config()
 c.InlineBackend.rc = {}
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
