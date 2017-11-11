d718bb5b6ac94507ec28618017a59f0944dcdce1
deepak <dpak.cherian@gmail.com>
2017-11-10 16:26:31 -0800

    devilspie now knows Signal Beta.

--- devilspie2.lua
+++ devilspie2.lua
@@ -3,7 +3,7 @@
 debug_print("Window Name: " .. get_window_name());
 debug_print("Application name: " .. get_application_name())
 
-if (get_application_name() == "Rhythmbox") or (get_application_name() == "Signal Private Messenger") or (get_application_name() == "emacs")then
+if (get_application_name() == "Rhythmbox") or (get_application_name() == "Signal Beta") or (get_application_name() == "emacs")then
    pin_window();
 end
 
