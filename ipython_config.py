ef60abb6501c210f9aa090f8c9ec4aad48403aef
deepak <dpak.cherian@gmail.com>
2017-12-01 20:47:48 -0800

    Clean up ipython configs.

--- ipython_config.py
+++ ipython_config.py
@@ -1,6 +1,8 @@
 c = get_config()
 c.InlineBackend.rc = {}
-c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.extensions = [
+    'autoreload',
+    'line_profiler',
+    'memory_profiler']
 c.InteractiveShellApp.exec_lines = []
-c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
