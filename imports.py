c0d40f6751bd4fe1c464ec2a3cb68e83b48072e7
dcherian <deepak@cherian.net>
2019-03-15 14:35:14 -0600

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,5 +1,15 @@
+import os
 import sys
-sys.path.append('/home/deepak/python/')
+
+hostname = os.uname()[1]
+if hostname == 'darya':
+    home = '/home/deepak/'
+elif 'cheyenne' in hostname:
+    home = '/glade/u/home/dcherian/'
+elif 'caguas' in hostname:
+    home = '/Users/dcherian/'
+
+sys.path.append(home + 'python/')
 
 import warnings
 
@@ -21,7 +31,6 @@ xr.set_options(keep_attrs=True)
 import netCDF4 as nc
 import seawater as sw
 # import gsw
-import bottleneck as bn
 
 # for company python completer
 warnings.simplefilter("ignore",
@@ -29,70 +38,24 @@ warnings.simplefilter("ignore",
                       lineno=1943)
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
-import h5py
 
 import xrscipy as xrsp
 import sciviscolor as svc
-import moor
-import chipy
-import moorarr
 import xfilter
 
 import dcpy.xr_accessor
 
 # projects
-import sys
-sys.path.append('/home/deepak/work/eddydiff/')
-import eddydiff as ed
+if hostname in ['darya', 'cgdm-caguas']:
+    sys.path.append(home + 'work/eddydiff/')
+    sys.path.append(home + 'work/bay/scripts')
+    import eddydiff as ed
 
-sys.path.append('/home/deepak/work/bay/scripts')
-import bay
+    import bay
+    import moor
+    import chipy
 
 import dcpy.util
 import dcpy.ts
 import dcpy.oceans
 import dcpy.plots
-import dcpy.explode
-
-warnings.resetwarnings()
-
-warnings.filterwarnings("ignore", message="BlockingKernelClient._ip_changed")
-warnings.filterwarnings("ignore", message="ResourceWarning")
-warnings.filterwarnings("ignore", message="Completer")
-
-
-def export_bokeh(plot, outPNG, outJS, outHTML, bkjs='local'):
-    from bokeh.io import export_png
-    from bokeh.embed import autoload_static, file_html
-    import bokeh.resources
-
-    if bkjs is 'local':
-        # use local installed bokeh scripts
-        wherebokeh = bokeh.resources.Resources(mode='absolute')
-
-    if bkjs is 'remote' or bkjs is 'CDN':
-        # use CDN bokeh scripts
-        wherebokeh = bokeh.resources.CDN
-
-    if bkjs is 'relative':
-        # bkjs is a relative path?
-        wherebokeh = bokeh.resources.Resources(mode='relative', root_dir='./bokeh/')
-
-    if bkjs is 'inline':
-        wherebokeh = bokeh.resources.INLINE
-
-    # save the png file
-    export_png(plot, filename=outPNG)
-
-    # save the html file
-    with open(outHTML, "w") as writer:
-        writer.write(file_html(plot, wherebokeh, None))
-
-    js, script = autoload_static(plot, wherebokeh, outJS)
-
-    # save the .js file
-    with open(outJS, "w") as writer:
-        writer.write(js)
-
-    # embed in the org-exported HTML file
-    print('''#+BEGIN_EXPORT html\n{script}\n#+END_EXPORT'''.format(script=script.lstrip()))
