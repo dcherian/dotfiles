77c4806a86e5558883e4a09e1490bd0d885dd39e
dcherian <deepak@cherian.net>
2019-09-07 13:49:34 -0600

    Update ipython imports

--- imports.py
+++ imports.py
@@ -9,27 +9,20 @@ elif 'cheyenne' in hostname or 'casper' in hostname:
 elif 'caguas' in hostname:
     home = '/Users/dcherian/'
 
-sys.path.append(home + 'python/')
+# sys.path.append(home + 'python/')
 
 import warnings
 
-import importlib
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import cmocean
 
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 import numpy as np
 import scipy as sp
 import pandas as pd
-import seaborn as sns
 
 import xarray as xr
 xr.set_options(keep_attrs=True)
-
-import netCDF4 as nc
-import seawater as sw
-# import gsw
 
 # for company python completer
 warnings.simplefilter("ignore",
@@ -38,23 +31,12 @@ warnings.simplefilter("ignore",
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import xrscipy as xrsp
-import sciviscolor as svc
-import xfilter
-
-import dcpy.xr_accessor
-
 # projects
 if hostname in ['darya', 'cgdm-caguas']:
     sys.path.append(home + 'work/eddydiff/')
     sys.path.append(home + 'work/bay/scripts')
-    import eddydiff as ed
+    # import eddydiff as ed
 
-    import bay
-    import moor
-    import chipy
-
-import dcpy.util
-import dcpy.ts
-import dcpy.oceans
-import dcpy.plots
+    # import bay
+    # import moor
+    # import chipy
