383293a105f22e5076e789d68e911e4ea43eccd4
dcherian <deepak@cherian.net>
2022-12-07 19:52:32 -0700

    autoreload 1

--- ipython_config.py
+++ ipython_config.py
@@ -1,6 +1,6 @@
 c = get_config()
 # c.InlineBackend.rc = {}
 c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines.append("%autoreload 2")
+c.InteractiveShellApp.exec_lines.append("%autoreload 1")
 c.IPCompleter.use_jedi = False
 c.InlineBackend.figure_format = "retina"
